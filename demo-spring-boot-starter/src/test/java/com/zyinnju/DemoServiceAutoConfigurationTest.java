fa72a0ba19674dda65b0dba179ece7c5b39dc8ee
Martin7-1 <1754350460@qq.com>
2023-06-30 15:45:37 +0800

    feat: 添加 Condition Report Log

--- demo-spring-boot-starter/src/test/java/com/zyinnju/DemoServiceAutoConfigurationTest.java
+++ demo-spring-boot-starter/src/test/java/com/zyinnju/DemoServiceAutoConfigurationTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.logging.ConditionEvaluationReportLoggingListener;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
 /**
@@ -18,8 +20,10 @@ class DemoServiceAutoConfigurationTest {
     @Test
     void testBeanExist() {
         // 测试在有 demo.enabled=true 的情况下是否有 DemoService 的 Bean
-        contextRunner.withPropertyValues("demo.enabled=true").run(context -> Assertions.assertDoesNotThrow(() -> context.getBean(DemoService.class)));
+        contextRunner.withInitializer(new ConditionEvaluationReportLoggingListener(LogLevel.INFO))
+                .withPropertyValues("demo.enabled=true").run(context -> Assertions.assertDoesNotThrow(() -> context.getBean(DemoService.class)));
         // 测试在有 demo.enabled=false 的情况下是否有 DemoService 的 Bean
-        contextRunner.withPropertyValues("demo.enabled=false").run(context -> Assertions.assertThrows(BeansException.class, () -> context.getBean(DemoService.class)));
+        contextRunner.withInitializer(new ConditionEvaluationReportLoggingListener(LogLevel.INFO))
+                .withPropertyValues("demo.enabled=false").run(context -> Assertions.assertThrows(BeansException.class, () -> context.getBean(DemoService.class)));
     }
 }
